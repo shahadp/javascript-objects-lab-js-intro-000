5f7bfac7415174898421bea0011bc0cf184fecc3
Shahad Puthukudi <shahadp@gmail.com>
2020-05-08 22:46:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function updateObjectWithKeyAndValue(object, key, value)
 {
-   var newObject=Object.assign({},object)
-   newObject[key]=value
+   var newObject=Object.assign(object,{[key]:value})
+   //newObject[key]=value
    return newObject
 }
 
@@ -9,3 +9,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return obj
 }
+
+function deleteFromObjectByKey(object, key)
+
